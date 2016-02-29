159cadc951d4891fc1e21891e36e8b1b8bd7a340
Roy <rikkyroy@gmail.com>
2016-02-28 22:09:06 -0500

    Initial Demo
    Signed-off-by: Roy <rikkyroy@gmail.com>

---
+++ Assets/Resources/Scripts/CamFollow.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CamFollow : MonoBehaviour {
+
+	public GameObject target;
+
+	private Transform container;
+	// Use this for initialization
+	void Start () {
+		container = this.transform.parent;
+		ParentToPlayer ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void ParentToPlayer() {
+		this.transform.SetParent (target.transform);
+	}
+
+	public void ParentToContainer() {
+		this.transform.SetParent (container);
+	}
+}
---
+++ Assets/Resources/Scripts/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	// Update is called once per frame
+	void Update () {
+		
+		KeyMovement ();
+
+	}
+
+	public float keySpeed = 5.0f; 
+	public float keyRotation = 10.0f;
+	public float keyAcceleration = 0.2f;
+	public float keyMaxSpeed = 50.0f;
+
+	void KeyMovement() {
+		if (Input.GetKey (KeyCode.W)) {
+			if (keySpeed < keyMaxSpeed) {
+				keySpeed += keyAcceleration;
+			}
+
+		} 
+		if (Input.GetKey (KeyCode.A)) {
+			this.transform.Rotate (0.0f, -keyRotation, 0.0f);
+
+		} 
+		if (Input.GetKey (KeyCode.S)) {
+			if (keySpeed > 0.0f)
+				keySpeed -= keyAcceleration; 
+
+		} 
+		if (Input.GetKey (KeyCode.D)) {
+			this.transform.Rotate (0.0f, keyRotation, 0.0f);
+
+		}
+
+		Vector3 dir = new Vector3 (Camera.main.transform.forward.x, 0.0f, Camera.main.transform.forward.z);
+		this.transform.Translate (dir * Time.deltaTime * keySpeed, Space.World);
+
+	}
+}
